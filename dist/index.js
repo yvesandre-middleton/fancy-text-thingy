4efd814febf842005338dacb3128e444bd496822
yvesandre-middleton <yvesmiddleton@gmail.com>
2018-03-15 14:20:54 +0000

    fixed babel and added .babelrc file

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
-let fancyText = () => 'hello world';
+let React = require('react');
+
+let fancyText = () => React.createElement(
+  'div',
+  null,
+  'hello world'
+);
 module.exports = fancyText;
